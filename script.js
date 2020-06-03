286d0c0a17e263f64440ac7048e647b291ba5e5b
DebAxom <64824663+DebAxom@users.noreply.github.com>
2020-06-03 09:23:44 +0530

    shusrusa

---
+++ script.js
@@ -0,0 +1,1 @@
+function services(){if(document.getElementById("menulist").style.display=="none") {document.getElementById("menulist").style.display="block";}else{document.getElementById("menulist").style.display="none";}}
