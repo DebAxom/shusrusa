83bc860f8c25741075952e353aa9b30f0444eecc
DebAxom <64824663+DebAxom@users.noreply.github.com>
2020-06-06 11:20:59 +0530

    shusrusa

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 function services(){if(document.getElementById("menulist").style.display=="none") {document.getElementById("menulist").style.display="block";}else{document.getElementById("menulist").style.display="none";}}
+var img = ['shusrusa.png','hospital.png','nalbari.png'];
+        var i= 0;
+        function slide(){
+        var imgsrc = document.getElementById("imgslider").src= img[i];
+        if (i<(img.length-1)){i++;}else{i=0;}
+                       }
+      setInterval(slide,1000);
